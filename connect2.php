d3c5e060dca11e108eeeaaeadd71d3450d07e45e
AnnaSpearman <55716945+AnnaSpearman@users.noreply.github.com>
2019-10-28 19:10:28 -0400

    Update connect2.php

--- connect2.php
+++ connect2.php
@@ -23,6 +23,51 @@
 		  $data = htmlspecialchars($data);
 		  return $data;
 		}
+//Create a new PHPMailer instance
+$mail = new PHPMailer;
+//Tell PHPMailer to use SMTP
+$mail->isSMTP();
+//Enable SMTP debugging
+// SMTP::DEBUG_OFF = off (for production use)
+// SMTP::DEBUG_CLIENT = client messages
+// SMTP::DEBUG_SERVER = client and server messages
+$mail->SMTPDebug = SMTP::DEBUG_SERVER;
+//Set the hostname of the mail server
+$mail->Host = 'smtp.gmail.com';
+// use
+// $mail->Host = gethostbyname('smtp.gmail.com');
+// if your network does not support SMTP over IPv6
+//Set the SMTP port number - 587 for authenticated TLS, a.k.a. RFC4409 SMTP submission
+$mail->Port = 587;
+//Set the encryption mechanism to use - STARTTLS or SMTPS
+$mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
+//Whether to use SMTP authentication
+$mail->SMTPAuth = true;
+//Username to use for SMTP authentication - use full email address for gmail
+$mail->Username = 'litkitsco@gmail.com';
+//Password to use for SMTP authentication
+$mail->Password = 'litkits2020!!';
+//Set who the message is to be sent from
+$mail->setFrom('litkitsco@gmail.com', 'LitKits');
+//Set an alternative reply-to address
+$mail->addReplyTo('litkitsco@gmail.com', 'LitKits');
+//Set who the message is to be sent to
+$mail->addAddress('litkitsco@gmail.com', 'LitKits');
+//Set the subject line
+$mail->Subject = 'Contact Us request';
+$mail->Body = $_POST["name"] $_POST["email"]  $_POST["message"];
+//Attach an image file
+$mail->addAttachment('images/phpmailer_mini.png');
+//send the message, check for errors
+if (!$mail->send()) {
+    echo 'Mailer Error: '. $mail->ErrorInfo;
+} else {
+    echo 'Message sent!';
+ 
+	}
+    }
+}
+
 
 
 $conn = pg_connect("host=ec2-54-83-55-125.compute-1.amazonaws.com port=5432 dbname=dfhnip1bkpfplp user=pywlzaoqipszkz password=0cef79548840ab44a871e15280ac8d12856f411749c240719c6d6c803010cfc9");
