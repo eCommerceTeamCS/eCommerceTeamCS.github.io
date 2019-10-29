551fdda11f7e456645b4b6def02276040cd737c3
shaynecassidy <55716957+shaynecassidy@users.noreply.github.com>
2019-10-28 20:57:02 -0400

    Update connect2.php

--- connect2.php
+++ connect2.php
@@ -58,8 +58,7 @@ $mail->addReplyTo('litkitsco@gmail.com', 'LitKits');
 $mail->addAddress('litkitsco@gmail.com', 'LitKits');
 //Set the subject line
 $mail->Subject = 'Contact Us request';
-//$mail->Body = $_POST["name"] $_POST["email"]  $_POST["message"];
-$mail->Body = 'Test';
+$mail->Body = $_POST["name"]+$_POST["email"]+$_POST["message"];
 //Attach an image file
 $mail->addAttachment('images/phpmailer_mini.png');
 //send the message, check for errors
