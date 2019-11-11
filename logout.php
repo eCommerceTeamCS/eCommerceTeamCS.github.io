f48bd5dbd6e72135f07db62ae781ff19fb96abe0
shaynecassidy <55716957+shaynecassidy@users.noreply.github.com>
2019-11-11 10:56:16 -0500

    Update logout.php

--- logout.php
+++ logout.php
@@ -1,6 +1,5 @@
 <?php
-  session_name( 'Member' );
-	session_start(); 
+  session_start(); 
   session_destroy();
-  header("Location: https://lit-kits.herokuapp.com/index.html");
+  header("Location: index.html");
 ?>
